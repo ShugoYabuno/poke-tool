0b7c105d0a08348f42ab33433e7c48081f7a71aa
ShugoYabuno <shugo.ybn@gmai.com>
2021-09-11 15:27:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# nuxt_firebase_starter
+
+## Build Setup
+
+```bash
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# remove git
+$ sudo rm -r .git
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ scripts/format.ts
@@ -0,0 +1,51 @@
+const fs = require("fs")
+
+for(let i = 1; i <= 8; i++) {
+  const gen: Record<string, unknown>[] = require(`../static/original/pokemon-data.json/jp/gen${i}-jp.json`)
+
+  const evolutions: {
+    name: string
+    eggMoves: string[]
+  }[] = []
+  const formatted = gen.map((_pokemon: any) => {
+    const abilities = [...new Set(_pokemon.abilities)]
+    const levelUpMoves = _pokemon.level_up_moves.map((_move: any) => _move[1])
+    const tms = _pokemon.tms.map((_move: any) => _move[1])
+
+    const eggMoves: string[] = _pokemon.egg_moves
+    const findEvolution = evolutions.find(_evolution => _evolution.name === _pokemon.name)
+    if(findEvolution) {
+      eggMoves.concat(findEvolution.eggMoves)
+    }
+
+    const moves = [...new Set([...levelUpMoves, ...tms, ...eggMoves])]
+
+    if(eggMoves.length >= 1 && _pokemon.evolutions.length >= 1) {
+      _pokemon.evolutions.forEach((_evolution: any) => {
+        const evolution = {
+          name: _evolution.species.replace(/-\d/, ""),
+          eggMoves
+        }
+        evolutions.push(evolution)
+      })
+    }
+
+    return {
+      name: _pokemon.name,
+      types: _pokemon.types,
+      baseStats: {
+        hitPoint: _pokemon.base_stats[0],
+        attack: _pokemon.base_stats[1],
+        defense: _pokemon.base_stats[2],
+        spAttack: _pokemon.base_stats[3],
+        spDefense: _pokemon.base_stats[4],
+        speed: _pokemon.base_stats[5],
+        total: _pokemon.base_stats[6]
+      },
+      abilities,
+      moves
+    }
+  })
+
+  fs.writeFileSync(`./static/processed/pokemon/v1/gen${i}.json`, JSON.stringify(formatted))
+}
